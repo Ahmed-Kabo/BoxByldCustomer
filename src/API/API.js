0b273c7233c81033edbd7dae77c8ba88282ebfb9
Ahmed Kabo <ahmedabuelnaga25@gmail.com>
2022-09-13 23:24:11 +0200

    update api

--- src/API/API.js
+++ src/API/API.js
@@ -1,5 +1,5 @@
 //the mean URL for API
-export const MAIN_URL = `http://kustomers.boxbyld.tech/api`;
+export const MAIN_URL = `https://kustomers.boxbyld.tech/api`;
 
 //login
 export const LOGIN_URL = `${MAIN_URL}/login`;
